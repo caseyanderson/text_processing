9a43af972ad5a3d982c06a88334923c886fc3e5f
caseyanderson <casey.thomas.anderson@gmail.com>
2017-01-29 15:07:28 -0800

    added funcs file and usage example

---
+++ inaugural_address_analysis_funcs.py
@@ -0,0 +1,76 @@
+# from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+
+
+# takes a url as input, outputs a soup object
+def web2soup(link):
+    url = urlopen(str(link))
+    soup = BeautifulSoup(url, 'html.parser')
+    return soup
+
+# takes a filtered soup object as input, returns only text content from html, strips newline chars, outputs list
+def soup2text(soup):
+    text = []
+
+    for i in soup:
+        text.append(i.text.strip())
+    return text
+
+# takes a list item as input, uses a speech start word to cut out intro (etc.), and outputs one string (justspeech)
+def list2string(corpus_list, start_word):
+    start = None
+
+    allofspeech = ''.join(corpus_list) # converts list items to one long string
+    pos = allofspeech.find(str(start_word))
+    start = pos - 1
+    justspeech = allofspeech[start: ]
+    return justspeech
+
+# takes a string as input, outputs a list of words
+def tokenizer(corpus):
+    tokenized = nltk.word_tokenize(corpus)
+    return tokenized
+
+# takes a list of words as input, returns a new list with no punctuation
+def nopunc(tokenized):
+    corpus = []
+    step = 0
+
+    for i in tokenized:
+        print('position ' + str(step))
+        if re.search('[^A-Za-z0-9]+', i) is not None:
+            print(str(i) + ' is a symbol, skip!')
+        else:
+            print(str(i) + ' is not a symbol, keep!')
+            corpus.append(str(i))
+        step+=1
+    return corpus
+
+# takes a word list as input, returns a new tuple with parts of speech tags
+def posTag(corpus):
+    words = nltk.pos_tag(corpus)
+    return words
+
+# takes a tuple and splits it into two lists
+def tupleSplitter(corpus):
+    a,b = zip(*corpus)
+    a = list(a)
+    b = list(b)
+    return a, b
+
+# takes corpus, pos tag lists and dictionary of parts of speech tag terms, uses pos to choose tags, outputs parts of speech only
+def posFilter(corpus, tagged, pos, dct ):
+    step = 0
+    size = len(tagged)
+    posCorpus = []
+    x = dct[str(pos)]
+
+    for i in x:
+        step = 0
+        for j in tagged:
+            if i == j:
+                posCorpus.append(corpus[step])
+                step = step + 1
+            else:
+                step = step + 1
+    return posCorpus
---
+++ usage.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+import nltk
+
+tags={}
+tags['noun'] = [ 'NN', 'NNP', 'NNPS', 'NNS']
+tags['adj'] = ['JJ', 'JJR', 'JJS']
+tags['vrb'] = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+tags['advrb'] = ['RB', 'RBR', 'RBS']
+tags['pronoun'] = ['PRP', 'PRP']
+
+
+speech = web2soup('http://abcnews.go.com/Politics/full-text-president-donald-trumps-inauguration-speech/story?id=44915821')
+
+# for Trump Inaugural Address
+soup = speech.find_all('p')
+
+
+html = soup2text(soup)
+
+text = list2string(html, "Chief")
+
+words = tokenizer(text)
+
+soup = nopunc(words)
+
+pos = posTag(soup)
+
+a = tupleSplitter(pos)
+
+filtered = posFilter(a[0], a[1], 'pronoun', tags)
